a6ceae2b9186c1219a3af7e1465caca24adc0dd1
ZhenhuiHe15 <herbertynwa@outlook.com>
2024-07-04 23:01:44 +1000

    test5.0

--- weather-app/app/api/bigdays/route.ts
+++ weather-app/app/api/bigdays/route.ts
@@ -2,13 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
   try {
     const apiKey = process.env.OPENWEATHERMAP_API_KEY;
     if (!apiKey) {
       return new Response('API key is not set', { status: 500 });
     }
 
-    const searchParams = req.nextUrl.searchParams;
     const lat = searchParams.get('lat');
     const lon = searchParams.get('lon');
 
